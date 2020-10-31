8076468edbaf92e688a2a6ffa951dd0889a4ddfd
Guz9ka <60352331+Guz9ka@users.noreply.github.com>
2020-10-31 15:57:39 +0300

    5.3
    Добавил платформу и разрушение блоков платформы

--- Assets/Scripts/FallableBlock.cs
+++ Assets/Scripts/FallableBlock.cs
@@ -2,17 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum BlockState
+{
+    Static,
+    Touched,
+    Fall
+}
+
 public class FallableBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private BlockState blockState;
+
+    [Header("Задержка между выполнениями действий")]
+    public float fallDelay;
+    public float destroyDelay;
+
+    [Header("Скорость падения")]
+    public float fallSpeed;
+
+    private void Start()
     {
-        
+        blockState = BlockState.Static;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Player" || other.tag == "Enemy" && blockState == BlockState.Static)
+        {
+            blockState = BlockState.Touched;
+            StartCoroutine(StartDestroy());
+        }
+    }
+
+    private IEnumerator StartDestroy()
+    {
+        //активировать визуальный эффект
+
+        yield return new WaitForSeconds(fallDelay);
+
+        blockState = BlockState.Fall;
+
+        yield return new WaitForSeconds(destroyDelay);
+
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if(blockState == BlockState.Fall) 
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, Mathf.Lerp(gameObject.transform.position.y, 0, fallSpeed * Time.deltaTime), gameObject.transform.position.z);
+        }
     }
 }
