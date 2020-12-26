b7a7ee8725f652841954014d5e1adf8cfd9de26f
Guz9ka <60352331+Guz9ka@users.noreply.github.com>
2020-12-27 01:40:25 +0300

    changed wheel settings

--- Assets/CarSteeringAE86.cs
+++ Assets/CarSteeringAE86.cs
@@ -13,22 +13,48 @@ public class CarSteeringAE86 : MonoBehaviour
     [Header("Engine parametres")]
     [SerializeField] private int maxWheelTurnAngle;
     [SerializeField] private float maxTorque;
-    [SerializeField] private float driftForce;
+
+    [SerializeField] private float rearWheelsRotationScale;
 
     private float _inputVertical;
     private float _inputHorizontal;
+
+    [SerializeField] private float breakingForce;
+    [SerializeField] private float breakingWheelRotation;
+    private bool _isBraking;
+
+    private Rigidbody carRigidbody;
+
+    private void Start()
+    {
+        carRigidbody = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         GetInput();
         AddTorque();
         RotateWheels();
+        ResetTorque();
+        Brake();
+    }
+
+    private void ResetTorque()
+    {
+        if (carRigidbody.velocity.z < 0.2 && carRigidbody.velocity.z > -0.2 && _inputVertical < 0.2 && _inputVertical > 0.2)
+        {
+            wheelRR.motorTorque = 0;
+            wheelRL.motorTorque = 0;
+        }
     }
 
     private void GetInput()
     {
-        _inputVertical = Input.GetAxis(axisName:"Vertical");
+        _inputVertical = Input.GetAxis(axisName: "Vertical");
         _inputHorizontal = Input.GetAxis(axisName: "Horizontal");
+
+        _isBraking = Input.GetKey(KeyCode.Space);
+        Debug.Log(_isBraking);
     }
 
     private void AddTorque()
@@ -41,5 +67,17 @@ public class CarSteeringAE86 : MonoBehaviour
     {
         wheelFR.steerAngle = maxWheelTurnAngle * _inputHorizontal;
         wheelFL.steerAngle = maxWheelTurnAngle * _inputHorizontal;
+        
+        //wheelRR.steerAngle = maxWheelTurnAngle * _inputHorizontal / rearWheelsRotationScale * -1;
+        //wheelRL.steerAngle = maxWheelTurnAngle * _inputHorizontal / rearWheelsRotationScale * -1;
+
+    }
+
+    private void Brake()
+    {
+        wheelFR.brakeTorque = _isBraking ? breakingForce : 0;
+        wheelFL.brakeTorque = _isBraking ? breakingForce : 0;
+
+        //wheelFR.sidewaysFriction.
     }
 }
